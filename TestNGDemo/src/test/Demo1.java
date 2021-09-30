9f1691bf2afd52778bd48994a23f36f378b0cacb
haky1313 <haky1@LAPTOP-9T5F1S0J>
2021-09-30 17:51:41 +0530

    first commit

---
+++ TestNGDemo/src/test/Demo1.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void demo() {
+		// TODO Auto-generated method stub
+		System.out.println("harkirat");
+	}
+
+}
