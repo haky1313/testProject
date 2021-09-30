425c8d292a14ba30332b84cb8cba7656aa706c45
haky1313 <haky1@LAPTOP-9T5F1S0J>
2021-09-30 18:06:46 +0530

    first comit project

---
+++ MavenJava/src/test/java/hakyMaven/MavenJava/ChromeLoadTest.java
@@ -0,0 +1,18 @@
+package hakyMaven.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class ChromeLoadTest {
+
+	@Test
+	public void loadChromeDriver()
+	{
+		System.out.println("Loading Chrome Driver....");
+	}
+	
+	@Test
+	public void loginChromeTest()
+	{
+		System.out.println("Loging in from Chrome....");
+	}
+}
---
+++ MavenJava/src/test/java/hakyMaven/MavenJava/IELoadTest.java
@@ -0,0 +1,18 @@
+package hakyMaven.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class IELoadTest {
+
+	@Test
+	public void loadIEDriver()
+	{
+		System.out.println("Loading IE Driver....");
+	}
+	
+	@Test
+	public void loginIETest()
+	{
+		System.out.println("Loging in from IE....");
+	}
+}
